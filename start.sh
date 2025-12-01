57aa9d605afc7ea998a2b4857fa9411954030eb5
grot-pixel <garrett.walshe@gmail.com>
2025-12-01 16:47:06 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# start.sh -- start the bot locally (make sure env vars set)
+# start.sh - start the bot locally (make sure env vars set)
 export APCA_API_KEY_1=${APCA_API_KEY_1:-""}
 export APCA_API_SECRET_1=${APCA_API_SECRET_1:-""}
-export APCA_BASE_URL_1=${APCA_BASE_URL_1:-""}
-python bot.py
+export APCA_BASE_URL_1=${APCA_BASE_URL_1:-"https://paper-api.alpaca.markets"}
+python bot.py --once
